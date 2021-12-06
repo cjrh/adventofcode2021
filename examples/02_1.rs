be5a0bba45c0fa1b64c88acab03bd8fc608565ff
Caleb Hattingh <caleb.hattingh@gmail.com>
2021-12-06 21:59:33 +1000

    Preparing for day 2 puzzles

---
+++ examples/02_1.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let puzzle1_input = adventofcode2021::get_input_data("input02_test.txt");
+    count_increases(&puzzle1_input);
+}
+
+fn count_increases(input: &str) -> i32 {
+    let v = input
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut increases = 0;
+    let mut prev = 1e8 as i32;
+    for x in &v {
+        if x > &prev {
+            increases += 1;
+        };
+        prev = *x;
+    }
+    println!("increases: {}", increases);
+    0
+}
+
